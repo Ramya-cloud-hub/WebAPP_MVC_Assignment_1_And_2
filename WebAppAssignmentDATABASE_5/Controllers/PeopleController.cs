69030ae692298d81632f7ac74b9a2c2297a0856f
Ramya-cloud-hub <ramya118gowda@gmail.com>
2021-09-06 07:17:05 +0200

    Add project files.

---
+++ WebAppAssignmentDATABASE_5/Controllers/PeopleController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppAssignmentDATABASE_5.Models;
+using WebAppAssignmentDATABASE_5.Models.ViewModel;
+
+namespace WebAppAssignmentDATABASE_5.Controllers
+{
+    public class PeopleController : Controller
+    {
+
+ 
+        [HttpGet]
+        public IActionResult Index()
+        {
+     
+           PeopleService checkListView = new PeopleService();
+            PeopleViewModel peopleList = new PeopleViewModel();
+
+            InMemoryPeopleRepo makeBaseList = new InMemoryPeopleRepo();
+
+            peopleList.PeopleListView = checkListView.All().PeopleListView;
+
+            if (peopleList.PeopleListView.Count == 0 || peopleList.PeopleListView == null)
+            {
+                makeBaseList.CreateBasePersons();
+            }
+
+            return View(peopleList);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Index(PeopleViewModel peopleviewModel)
+        {
+            PeopleService filterString = new PeopleService();
+
+            peopleviewModel = filterString.FindBy(peopleviewModel);
+            return View(peopleviewModel);
+        }
+
+        [HttpGet]
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePerson(CreatePersonViewModel personViewModel) 
+        {
+            var newPersonModel = new PeopleViewModel();
+            PeopleService peopleService = new PeopleService();
+
+            if (ModelState.IsValid)
+            {
+
+                newPersonModel.PersonName = personViewModel.PersonName;
+                newPersonModel.PersonPhoneNumber = personViewModel.PersonPhoneNumber;
+                newPersonModel.PersonCity = personViewModel.PersonCity;
+
+                newPersonModel.PeopleListView = peopleService.All().PeopleListView;
+
+
+                peopleService.Add(personViewModel);
+
+                ViewBag.Mess = "Person Added!";
+
+                return View("Index", newPersonModel);
+            }         
+
+            return View("index", newPersonModel);
+        }
+
+
+        public IActionResult DeletePerson(int id)
+        {
+            PeopleService deleteById = new PeopleService();
+            deleteById.Remove(id);
+
+            return RedirectToAction("Index");
+        }
+    
+
+    }
+}
---
+++ WebAppAssignmentDATABASE_5/Models/Repo/DatabasePeopleRepo.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppAssignmentDATABASE_5.Models
+{
+    public class DatabasePeopleRepo : IPeopleRepo
+    {
+        private static List<Person> _personList = new List<Person>();
+        private static int _idCounter;
+     
+
+        //Extra Code Added Last
+        public Person Create(string PersonName, string PersonPhoneNumber, string PersonCity)
+        {
+            Person newPerson = new Person(_idCounter, PersonName, PersonPhoneNumber, PersonCity);
+            _personList.Add(newPerson);
+
+            _idCounter++;
+
+            return newPerson;
+        }
+
+        public bool Delete(Person person)
+        {
+            _personList.Remove(person);
+            return true;
+        }
+
+        public List<Person> Read()
+        {
+            return _personList;
+        }
+
+        public Person Read(int id)
+        {
+            Person findPersonById = _personList.Find(idNr => idNr.PersonId == id);
+
+            return findPersonById;
+        }
+
+        public Person Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
